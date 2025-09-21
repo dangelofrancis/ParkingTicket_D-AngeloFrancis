5321444b39174279cabb5f1f9c4a9cb6001e095c
D'Angelo Francis <dangelofrancis9@gmail.com>
2025-09-21 02:54:02 -0400

    Added more test scenarios

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
-# TITLE: Parking Ticket Simulation
-# FILE NAME: ParkingTicket_D_AngeloFrancis.py
-# DATE: 9/21/2025
-# PROGRAMMER: D'Angelo Francis
-# REQUIREMENTS: Simulate police officer issuing parking tickets
+###################################################################
+# TITLE: Parking Ticket Simulation                                #
+# FILE NAME: ParkingTicket_D_AngeloFrancis.py                     #
+# DATE: 9/21/2025                                                 #
+# PROGRAMMER: D'Angelo Francis                                    #
+# REQUIREMENTS: Simulate police officer issuing parking tickets   #
+###################################################################
 
 from ParkingMeter import ParkingMeter
 from ParkedCar import ParkedCar
 from PoliceOfficer import PoliceOfficer
 from ParkingTicket import ParkingTicket
-
 
 ################
 #  SCENARIO 1  #
@@ -24,7 +25,8 @@ ticket = officer.inspect_car(car, meter)
 if ticket:
     print(ticket)
 else:
-    print("No violation. Car is parked legally.\n")
+    print("No violation. Car is parked legally.")
+print("\n")
 
 ################
 #  SCENARIO 2  #
@@ -40,7 +42,8 @@ ticket = officer.inspect_car(car, meter)
 if ticket:
     print(ticket)
 else:
-    print("No violation. Car is parked legally.\n")
+    print("No violation. Car is parked legally.")
+print("\n")
 
 ################
 #  SCENARIO 3  #
@@ -56,5 +59,31 @@ ticket = officer.inspect_car(car, meter)
 if ticket:
     print(ticket)
 else:
-    print("No violation. Car is parked legally.\n")
+    print("No violation. Car is parked legally.")
+print("\n")
 
+################
+#  SCENARIO 4  #
+################
+
+
+print("SCENARIO 4")
+
+officer = PoliceOfficer("Sarah Green", "9999")
+cars_meters = [
+    # legally parked
+    (ParkedCar("Nissan", "Altima", "White", "JKL321", 60),ParkingMeter(60)),
+    (ParkedCar("Mazda", "3", "Blue", "MAZ321", 45),ParkingMeter(60)),
+    # illegally parked
+    (ParkedCar("Chevy", "Malibu", "Silver", "QWE789", 80),ParkingMeter(60)),
+    (ParkedCar("BMW","X5", "Black", "BMW999", 500),ParkingMeter(60)),
+    (ParkedCar("Toyota", "Corolla", "Black", "TOY123", 90),ParkingMeter(60))] 
+
+for idx, (car, meter) in enumerate(cars_meters, 1):
+    print(f"Car #{idx}:")
+    ticket = officer.inspect_car(car, meter)
+    if ticket:
+        print(ticket)
+    else:
+        print("No violation. Car is parked legally.")
+    print("\n")
